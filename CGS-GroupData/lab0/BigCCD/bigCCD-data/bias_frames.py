392223fbdc4189832121ad296027c29b9abd4ab5
jgarani <jasmine.garani@gmail.com>
2017-09-13 18:09:33 -0400

    Edited master bias function

--- CGS-GroupData/lab0/BigCCD/bigCCD-data/bias_frames.py
+++ CGS-GroupData/lab0/BigCCD/bigCCD-data/bias_frames.py
@@ -10,61 +10,70 @@ jasminepath = "/Users/Jasmine/Documents/stony_brook/y4_sb/ast443/CGS-Groupdata/l
 lorenapath = "/home/icecube/AST/CGS-GroupData/lab0/BigCCD/bigCCD-data/"
 
 #Plot and fit bias
-hdulist = fits.open(lorenapath + '/' + "biases-Neg5-Vis.00000000.BIAS.FIT")
-header = hdulist[0].header
-print(header)
-imagedata = hdulist[0].data
-countvalues = imagedata.flatten()
-print("max counts = " + str(np.max(countvalues)))
-print("min counts = " + str(np.min(countvalues)))
+def plotbias(path,temp):
+    if temp == 'Neg5':
+        filename = 'biases-Neg5-Vis.00000000.BIAS.FIT'
+    if temp == 'Neg10':
+        filename = 'bias-Neg10-Vis.00000000.BIAS.FIT'
+    print jasminepath + '/' + filename
+    hdulist = fits.open(jasminepath + '/' + filename)
+    header = hdulist[0].header
+    print(header)
+    imagedata = hdulist[0].data
+    countvalues = imagedata.flatten()
+    print("max counts = " + str(np.max(countvalues)))
+    print("min counts = " + str(np.min(countvalues)))
 
-print("mean = " + str(np.mean(countvalues)))
-print("mode = " + str(stats.mode(countvalues)[0][0]))
-print("median = " + str(np.median(countvalues)))
-print("sigma = " + str(np.std(countvalues)))
+    print("mean = " + str(np.mean(countvalues)))
+    print("mode = " + str(stats.mode(countvalues)[0][0]))
+    print("median = " + str(np.median(countvalues)))
+    print("sigma = " + str(np.std(countvalues)))
 
-cmin = 900
-cmax = 1450
-nbins = 100
+    cmin = 900
+    cmax = 1450
+    nbins = 100
 
-normalization = (cmax-cmin)/nbins*len(countvalues[(countvalues>=cmin) & (countvalues<=cmax)])
+    normalization = (cmax-cmin)/nbins*len(countvalues[(countvalues>=cmin) & (countvalues<=cmax)])
 
-mean_single = np.mean(countvalues) #mean of single image
-sig_single = np.std(countvalues) #standard deviation of single image
+    mean_single = np.mean(countvalues) #mean of single image
+    sig_single = np.std(countvalues) #standard deviation of single image
 
-#fit gaussian
-xarray = np.linspace(cmin,cmax,nbins*10)
-yarray = normalization*norm.pdf(xarray,loc = mean_single,scale = sig_single)
-plt.hist(countvalues,range=[cmin,cmax],bins=nbins);
-plt.yscale("log")
-plt.ylim([0.1,1e6])
-plt.plot(xarray,yarray,color="red")
-#plt.show()
+    #fit gaussian
+    xarray = np.linspace(cmin,cmax,nbins*10)
+    yarray = normalization*norm.pdf(xarray,loc = mean_single,scale = sig_single)
+    plt.hist(countvalues,range=[cmin,cmax],bins=nbins);
+    plt.yscale("log")
+    plt.ylim([0.1,1e6])
+    plt.plot(xarray,yarray,color="red")
+    #plt.show()
 
-#set cut limimts for outliers
-cut_upper = mean_single+5*sig_single
-cut_lower = mean_single-5*sig_single
+    #set cut limimts for outliers
+    cut_upper = mean_single+5*sig_single
+    cut_lower = mean_single-5*sig_single
 
-clippedvalues = countvalues[(countvalues>=cut_lower) & (countvalues<=cut_upper)]
+    clippedvalues = countvalues[(countvalues>=cut_lower) & (countvalues<=cut_upper)]
 
-#calculate percent of pixels rejected by cut
-pix_reject = 100*((len(countvalues)-len(clippedvalues))/float(len(countvalues)))
-print("percent of pixels rejected = "+str(pix_reject))
+    #calculate percent of pixels rejected by cut
+    pix_reject = 100*((len(countvalues)-len(clippedvalues))/float(len(countvalues)))
+    print("percent of pixels rejected = "+str(pix_reject))
 
-#=============================================================================================
-#4.1.2
-#Convert measurement of the read noise into units of electrons
-gain = header['E-Gain']
-e_rn = sig_single*gain #readnoise in units of electrons
-e_rn_manufact = 14.8 #e-, readnoise from the manufacturer
-print("read noise in electron units = " + str(e_rn))
+    #=============================================================================================
+    #4.1.2
+    #Convert measurement of the read noise into units of electrons
+    gain = header['E-Gain']
+    e_rn = sig_single*gain #readnoise in units of electrons
+    e_rn_manufact = 14.8 #e-, readnoise from the manufacturer
+    print("read noise in electron units = " + str(e_rn))
+
+    return sig_single
 #=============================================================================================
 #4.1.3
 #make master bias, find mean and std
-def mastbineg5(path,temp): #path,temperature word in file name
+def mastbi(path,temp): #path,temperature word in file name
     all_bias = []
     for f in os.listdir(jasminepath):
         if "bias" in f and temp in f: #put all biases in an array
+            print f
             hdulist1 = fits.open(jasminepath + '/' + f)
             header1 = hdulist1[0].header
             imagedata1 = hdulist1[0].data
@@ -75,11 +84,17 @@ def mastbineg5(path,temp): #path,temperature word in file name
     countvalues1 = master_bias.flatten()
     mean_all = np.mean(countvalues1)
     sig_all = np.std(countvalues1)
+    sig_single = plotbias(path,temp)
+    print "Mean all = ", mean_all
+    print "Sig all = ", sig_all
+    N = len(all_bias) #number of images used for master bias
+    calc_sig_single = sig_single * (1/np.sqrt(N))
+    print "Calculated standard deviation of master_bias", calc_sig_single
     
-    
-
     return
 
 
-
-mastbineg5(jasminepath,"Neg5")
+#plotbias(jasminepath,"Neg5")
+#mastbi(jasminepath,"Neg5")
+plotbias(jasminepath,"Neg10")
+mastbi(jasminepath,"Neg10")
