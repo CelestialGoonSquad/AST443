4414e1cf223a9b4f8f5af357d69073a5adff4719
jgarani <jasmine.garani@gmail.com>
2017-09-13 20:43:19 -0400

    first upload of flat field code

---
+++ CGS-GroupData/lab0/BigCCD/bigCCD-data/flat_field.py
@@ -0,0 +1,44 @@
+#Jasmine Garani, Patrick Payne, Lorena Mezini
+#Start date 9/13/17
+
+import numpy as np
+from astropy.io import fits
+import os
+import warnings
+from astropy.utils.exceptions import AstropyWarning
+warnings.simplefilter('ignore',category=AstropyWarning)
+import matplotlib.pyplot as plt
+
+#==========================================================================
+
+jasminepath = "/Users/Jasmine/Documents/stony_brook/y4_sb/ast443/CGS-Groupdata/lab0/BigCCD/bigCCD-data/"
+
+def masterflat(path):
+    all_flats = []
+    hdulist1 = fits.open(jasminepath + '/' + 'flat-expos23s-Neg5-Vis.00000009.FIT', ignore_missing_end=True)
+    for f in os.listdir(jasminepath):
+        if "flat" in f and "FIT" in f:
+            print f
+            hdulist = fits.open(jasminepath + '/' + f,ignore_missing_end=True)
+            header = hdulist[0].header
+            imagedata = hdulist[0].data
+            norm_data = imagedata/np.median(imagedata)
+            all_flats.append(norm_data)
+    master_flat = np.median(all_flats,axis=0)
+    master_flat = master_flat/np.mean(master_flat)
+    #flathdu = fits.PrimaryHDU(master_flat)
+    #flathdu.writeto('master_flat.fits',clobber=True)
+    low_counts = 0.956508
+    high_counts = 1.03464
+    ratio = low_counts/high_counts
+    print ratio
+    countvalues = master_flat.flatten()
+    nbins = 100
+    plt.hist(countvalues,bins=nbins)
+    plt.yscale("log")
+    plt.xlim([0.8,1.2])
+    plt.show()
+
+
+
+masterflat(jasminepath)
