05dfe687e431ecee5fdb7e528751be931a96e2a1
jgarani <jasmine.garani@gmail.com>
2017-09-17 14:32:25 -0400

    added check for dead pixels

--- CGS-GroupData/lab0/BigCCD/bigCCD-data/flat_field.py
+++ CGS-GroupData/lab0/BigCCD/bigCCD-data/flat_field.py
@@ -26,6 +26,15 @@ def masterflat(path):
             all_flats.append(norm_data)
     master_flat = np.median(all_flats,axis=0)
     master_flat = master_flat/np.mean(master_flat)
+    std = np.std(master_flat)
+    mean = np.mean(master_flat)
+    print 'standard deviation = ', std
+    print mean - 5.*std
+
+    for i in range(0,len(master_flat[0])):
+        for k in range(0,len(master_flat[1])):
+            if master_flat[i][k] <= mean - 5.*std:
+                print i,k
     #flathdu = fits.PrimaryHDU(master_flat)
     #flathdu.writeto('master_flat.fits',clobber=True)
     low_counts = 0.956508
@@ -34,10 +43,10 @@ def masterflat(path):
     print ratio
     countvalues = master_flat.flatten()
     nbins = 100
-    plt.hist(countvalues,bins=nbins)
+    '''plt.hist(countvalues,bins=nbins)
     plt.yscale("log")
     plt.xlim([0.8,1.2])
-    plt.show()
+    plt.show()'''
 
 
 
