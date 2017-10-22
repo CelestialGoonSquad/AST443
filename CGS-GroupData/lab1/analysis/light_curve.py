592aa9b7f60a49ca2b9b0145edeb605d4e4955e3
jgarani <jasmine.garani@gmail.com>
2017-10-22 11:13:57 -0400

    we made the light curve

--- CGS-GroupData/lab1/analysis/light_curve.py
+++ CGS-GroupData/lab1/analysis/light_curve.py
@@ -7,7 +7,7 @@ patpath = '/Users/ilovealltigers/SBU_F17/AST_443/LAB1/CGS-GroupData/lab1/calibra
 lorenapath = '/home/lmezini/AST443/CGS-GroupData/lab1/calibration2'
 jasminepath = '/home/jgarani/AST443/CGS-GroupData/lab1/calibration2'
 
-path = patpath
+path = jasminepath
 
 #create function to calcualte scaled flux and error
 def flux_calc(flux,error):
@@ -22,6 +22,8 @@ sciname = []
 dates =[]
 filenames = []
 ignore = []         #array for lines to be ignored due to deviation
+fluxpretran = []
+errpretran = []
 
 #create lists of lists for variables for the 10 extra objects
 names = [[] for _ in range(10)] #each list has 10 lists in it for each object
@@ -33,28 +35,34 @@ for f in os.listdir(path):
     if "hd" in f and "txt" in f:
         #print f
         name = np.genfromtxt(str(path)+'/'+str(f),dtype = str,usecols = 0)
+        index177 = np.where(name == 'bdf-L1-sci.00000177.cat')
+        index177 = index177[0][0] # gets index of last file before transit
+        index503 = np.where(name == 'bdf-L1-sci.00000503.cat')
+        index503 = index503[0][0] # gets index of first file after transit
         RA = np.genfromtxt(str(path)+'/'+str(f),usecols = 1)
         DEC = np.genfromtxt(str(path)+'/'+str(f),usecols = 2)
         flux = np.genfromtxt(str(path)+'/'+str(f),usecols = 3)
         error = np.genfromtxt(str(path)+'/'+str(f),usecols = 4)
         
-        star_fscaled = flux_calc(flux,error)[0]
-        star_escaled = flux_calc(flux,error)[1]
         #open fits files to get time
         for i in range(len(name)):
-            if star_fscaled[i] < (1.0 - 3*star_escaled[i]) or star_fscaled[i] > (1.0 + 3*star_escaled[i]):
-                ignore.append(i)
-            else:
-                hdulist = fits.open(str(path)+'/'+str(name[i])[:-3]+'new',ignore_missing_end=True)
-                header = hdulist[0].header
-                date = header['DATE']
-                dates.append(date)
-                sciflux.append(star_fscaled[i])
-                scierr.append(star_escaled[i])
-                sciname.append(name[i])
+            hdulist = fits.open(str(path)+'/'+str(name[i])[:-3]+'new',ignore_missing_end=True)
+            header = hdulist[0].header
+            date = header['DATE']
+            dates.append(date)
+            sciflux.append(flux[i])
+            if i <= index177: 
+                fluxpretran.append(flux[i])
+                errpretran.append(error[i])
+            scierr.append(error[i])
+            sciname.append(name[i])
     #get names of txt files for other objects
     if "time_flux" in f and "hd" not in f and '~' not in f:
         filenames.append(f)
+
+baseline = np.mean(fluxpretran)
+errbase = np.mean(errpretran)
+print "baseline and error: ", baseline,errbase
 
 i = 0
 for f in filenames:
@@ -66,48 +74,70 @@ for f in filenames:
     error = np.genfromtxt(str(path)+'/'+str(f),usecols = 4)
     ref_fscaled, ref_escaled = flux_calc(flux,error) #scaled fluxes and errors for reference images
     j = 0
-    #add values to list of lists
     for j in range(len(name)):        
-        if ref_fscaled[i] < (1.0 - 3*ref_escaled[i]) or ref_fscaled[i] > (1.0 + 3*ref_escaled[i]):
-            ignore.append(i)
-        else:        
-            names[i].append(name[j])
-            fref[i].append(ref_fscaled[j])
-            eref[i].append(ref_escaled[j])
-            j = j +1
+#        if ref_fscaled[j] < (1.0 - 10*ref_escaled[j]) or ref_fscaled[j] > (1.0 + 10*ref_escaled[j]):
+        if ref_fscaled[j] < 0.8 or ref_fscaled[j] > 1.2:
+            if j not in ignore:                
+                ignore.append(j)
+                print ignore
+        names[i].append(name[j])
+        fref[i].append(ref_fscaled[j])
+        eref[i].append(ref_escaled[j])
+        j = j+1
+
     i = i+1
     t_refstar = []
-    
     for w in range(len(name)):
         t_refstar.append(w)
-    plt.plot(t_refstar, ref_fscaled,label = str(f))
-    plt.legend()
-plt.show()
 
-  
+
+    # removes problem children...actually just the problem images
+    t_refstar = np.delete(t_refstar,ignore)
+    ref_fscaled = np.delete(ref_fscaled,ignore)
+    #plt.plot(t_refstar, ref_fscaled,label = str(f))
+    #plt.legend()
+#plt.show()
+
+# removes the problem images... we've already dealt with the children :)
+sciflux = np.delete(sciflux,ignore)
+sciname = np.delete(sciname,ignore)
+for k in range(0,10):
+    names[k] = np.delete(names[k],ignore)
+    fref[k] = np.delete(fref[k],ignore)
+    eref[k] = np.delete(eref[k],ignore)
+
+
 #create list for averaged flux and error over non-sci objects
-us = []
-uerrs = []
+mus = []
+muerrs = []
+ri = []
+
 
 for k in range(len(names[1])):
     if names[1][k] == sciname[k]:
         #sum up scaled flux for other objects at specific times
-        unum = (fref[1][k]/(eref[1][k]**2))+(fref[2][k]/(eref[2][k]**2))+(fref[3][k]/(eref[3][k]**2))+(fref[4][k]/(eref[4][k]**2))+(fref[5][k]/(eref[5][k]**2))+(fref[6][k]/(eref[6][k]**2))+(fref[7][k]/(eref[7][k]**2))+(fref[8][k]/(eref[8][k]**2))+(fref[0][k]/(eref[0][k]**2))
-        udenom = (1.0/(eref[1][k]**2))+(1.0/(eref[2][k]**2))+(1.0/(eref[3][k]**2))+(1.0/(eref[4][k]**2))+(1.0/(eref[5][k]**2))+(1.0/(eref[6][k]**2))+(1.0/(eref[7][k]**2))+(1.0/(eref[8][k]**2))+(1.0/(eref[0][k]**2))
-        uerr = np.sqrt(1.0/udenom) 
-        us.append(unum/udenom)
-        uerrs.append(uerr)
+        munum = (fref[1][k]/(eref[1][k]**2))+(fref[2][k]/(eref[2][k]**2))+(fref[3][k]/(eref[3][k]**2))+(fref[4][k]/(eref[4][k]**2))+(fref[5][k]/(eref[5][k]**2))+(fref[6][k]/(eref[6][k]**2))+(fref[7][k]/(eref[7][k]**2))+(fref[8][k]/(eref[8][k]**2))+(fref[0][k]/(eref[0][k]**2))
+        mudenom = (1.0/(eref[1][k]**2))+(1.0/(eref[2][k]**2))+(1.0/(eref[3][k]**2))+(1.0/(eref[4][k]**2))+(1.0/(eref[5][k]**2))+(1.0/(eref[6][k]**2))+(1.0/(eref[7][k]**2))+(1.0/(eref[8][k]**2))+(1.0/(eref[0][k]**2))
+        muerr = np.sqrt(1.0/mudenom) 
+        mus.append(munum/mudenom)
+        muerrs.append(muerr)
     else:
         print names[1][k],sciname[k]
 
-print sciname[544]
+for i in range(len(sciflux)):  # calculates normalized ri
+    divtemp = sciflux[i]/(mus[i]*baseline)
+    ri.append(divtemp)
+print ri
+
+#print sciname[544]
 t = []
 for k in range(len(sciflux)):
-    print dates[k],sciflux[k],scierr[k],us[k],uerrs[k],sciflux[k]/us[k]
+    print k,dates[k],sciflux[k],scierr[k],mus[k],muerrs[k],sciflux[k]/mus[k]
 for k in range(len(sciflux)):
     t.append(k)
 
-plt.plot(t,sciflux)
-plt.plot(t,us)
+plt.plot(t,ri)
+#plt.plot(t,mus)
 plt.ylim(0.8,1.10)
+
 plt.show()
