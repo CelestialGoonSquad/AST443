fc999d68bd6ffde64641134a11719e0a00790a83
jgarani <jasmine.garani@gmail.com>
2017-11-07 13:38:44 -0500

    python script for plotting spectrum

---
+++ CGS-GroupData/lab2/forpyraf/neon/neon-spectrum.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  6 20:53:25 2017
+
+@author: Jasmine
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+with open("neon-all-images.cal.txt") as f:
+    lines = f.readlines()
+    x = [line.split()[0] for line in lines]
+    y = [line.split()[1] for line in lines]
+
+print x
+print y
+
+plt.plot(x,y)
+plt.show()
