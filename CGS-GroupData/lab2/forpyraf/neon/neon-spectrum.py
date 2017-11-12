52738a9146c2584c6f141d735ee1e065e6493b4b
jgarani <jasmine.garani@gmail.com>
2017-11-12 16:55:22 -0500

    not sure

--- CGS-GroupData/lab2/forpyraf/neon/neon-spectrum.py
+++ CGS-GroupData/lab2/forpyraf/neon/neon-spectrum.py
@@ -8,7 +8,7 @@ Created on Mon Nov  6 20:53:25 2017
 import numpy as np
 import matplotlib.pyplot as plt
 
-with open("neon-all-images.cal.txt") as f:
+with open("all-images.cal.txt") as f:
     lines = f.readlines()
     x = [line.split()[0] for line in lines]
     y = [line.split()[1] for line in lines]
