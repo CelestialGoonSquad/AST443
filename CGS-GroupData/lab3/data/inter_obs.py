18944c17773f04c2403c3cafdf570e8aed478840
jgarani <jasmine.garani@gmail.com>
2017-11-26 17:06:27 -0500

    its better now

--- CGS-GroupData/lab3/data/inter_obs.py
+++ CGS-GroupData/lab3/data/inter_obs.py
@@ -1,8 +1,10 @@
 import numpy as np
+from scipy.signal import argrelextrema
 import matplotlib.pyplot as plt
 
-f_time = np.genfromtxt("Base-Inter-55-05.txt",skip_header=7,usecols = 0)
-f_pot = np.genfromtxt("Base-Inter-55-05.txt",skip_header=7,usecols = 1)
+name = "Base-Inter-55-05.txt"
+f_time = np.genfromtxt(name,skip_header=7,usecols = 0)
+f_pot = np.genfromtxt(name,skip_header=7,usecols = 1)
 
 def gaussian(t,sigma):
     """ a gaussian kernel """
@@ -24,11 +26,49 @@ def do_convolve(t,sig,kernal):
 sigmas = [0.01,0.03,0.05,0.075,0.10]
 i = [0.2,0.4,0.6,0.8,1.0]
 for s in range(len(sigmas)):
-    smooth_pot = do_convolve(f_time,f_pot,gaussian(f_time,sigmas[s]))
-    smooth_pot = smooth_pot + i[s]
-    plt.plot(f_time,smooth_pot,label = str(sigmas[s]))
+    if "50-07" in name:
+        smooth_pot = do_convolve(f_time[:-1],f_pot[:-1],gaussian(f_time[:-1],sigmas[s]))
+        smooth_pot = smooth_pot + i[s]
+        plt.plot(f_time[:-1],smooth_pot,label = str(sigmas[s]))
+        maxInd = argrelextrema(smooth_pot, np.less)
+        minInd = argrelextrema(smooth_pot, np.greater)
+        m = smooth_pot[minInd]
+        r = smooth_pot[maxInd]
+        tr = f_time[maxInd]
+        tm = f_time[minInd]
+        for j in range(len(r)):
+            print "time,max vals = ",tr[j],r[j]
+        for j in range(len(m)):
+            print "time,min vals = ",tm[j],m[j]
+    else:
+        smooth_pot = do_convolve(f_time,f_pot,gaussian(f_time,sigmas[s]))
+        smooth_pot = smooth_pot + i[s]
+        plt.plot(f_time,smooth_pot,label = str(sigmas[s]))
+        maxInd = argrelextrema(smooth_pot, np.less)
+        minInd = argrelextrema(smooth_pot, np.greater)
+        m = smooth_pot[minInd]
+        r = smooth_pot[maxInd]
+        tr = f_time[maxInd]
+        tm = f_time[minInd]
+        for j in range(len(r)):
+            print "time,max vals = ",tr[j],r[j]
+        for j in range(len(m)):
+            print "time,min vals = ",tm[j],m[j]
+
+print "For True Vals"
+maxInd = argrelextrema(f_pot, np.less)
+minInd = argrelextrema(f_pot, np.greater)
+r = f_pot[maxInd]
+m = f_pot[minInd] 
+tr = f_time[maxInd]
+tm = f_time[minInd]
+for j in range(len(r)):
+    print "time,max vals = ",tr[j],r[j]
+for j in range(len(m)):
+    print "time,min vals = ",tm[j],m[j]
 
 plt.plot(f_time,f_pot,label = "true")
+plt.title("35-11")
 plt.legend()
-plt.savefig("guassian_smoothed_inter_obsv.png")
+#plt.savefig("gaussian_smoothed_inter_obsv_30-12.png")
 plt.show()
