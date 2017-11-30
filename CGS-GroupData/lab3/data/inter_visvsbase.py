6012e4bf6374bee96ee251fcd2e4c7c9ca2de4da
jgarani <jasmine.garani@gmail.com>
2017-11-30 17:57:46 -0500

    trying to make the sinc fit

--- CGS-GroupData/lab3/data/inter_visvsbase.py
+++ CGS-GroupData/lab3/data/inter_visvsbase.py
@@ -3,12 +3,28 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
-name = 'bsnsv.txt'
+name = 'bsnsv-again.txt'
 baseline = np.genfromtxt(name,usecols = 0)
 vis = np.genfromtxt(name,usecols = 1)
 
 plt.plot(baseline,vis, linestyle='none',marker='o')
 plt.xlabel(r'Baseline (cm)')
 plt.ylabel(r'Visibility')
+
+
+#sinc = sin(piBalpha)/piB)
+
+#define the sinc function
+def sinc_func(b,a):
+    return np.sin(np.pi*b*a)/(np.pi*b)
+
+
+popt, pcov = curve_fit(sinc_func,baseline,vis)
+print popt
+print pcov
+
+plt.plot(baseline, sinc_func(baseline,*popt), linestyle = '--')
+plt.savefig('sinc-fit.pdf',format='pdf')
 plt.show()
