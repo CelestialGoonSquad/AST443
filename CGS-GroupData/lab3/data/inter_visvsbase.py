6dff8430f59937959b65349980f2378894f9c3a2
jgarani <jasmine.garani@gmail.com>
2017-12-02 13:34:33 -0500

    sinc stuff

--- CGS-GroupData/lab3/data/inter_visvsbase.py
+++ CGS-GroupData/lab3/data/inter_visvsbase.py
@@ -5,9 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
-name = 'bsnsv-again.txt'
-baseline = np.genfromtxt(name,usecols = 0)
-vis = np.genfromtxt(name,usecols = 1)
+name = 'bsnvs.txt'
+baseline = np.genfromtxt(name,skip_header=1,usecols = 0)
+vis = np.genfromtxt(name,skip_header=1,usecols = 1)
+error = np.genfromtxt(name,skip_header=1,usecols=2)
 
 plt.plot(baseline,vis, linestyle='none',marker='o')
 plt.xlabel(r'Baseline (cm)')
